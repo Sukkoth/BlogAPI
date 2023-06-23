aec827f9b9ac6f0c1389b50f04ab73e68de45777
Sukkoth <suukootj@gmail.com>
2023-06-23 10:31:49 +0300

    updated for server

--- config/env.js
+++ config/env.js
@@ -1,4 +1,4 @@
-const env = require('dotenv').config()
+const env = require('dotenv').config();
 
 module.exports = {
     APP_PORT: process.env.APP_PORT || 3000,
@@ -7,5 +7,7 @@ module.exports = {
 
     APP_KEY: process.env.APP_KEY || 'cRfUjWnZr4u7x!A%',
 
-    DB_URL: process.env.DB_URL
-}
+    DB_URL:
+        process.env.DB_URL ||
+        'mongodb+srv://sukkoth:sukkoth@myblog.a5ccukp.mongodb.net/BlogAPI',
+};
