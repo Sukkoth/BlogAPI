272567d43d5eedeed04afb23655b581827b21f3e
Sukkoth <99675293+Sukkoth@users.noreply.github.com>
2023-06-23 10:27:11 +0300

    Update env.js

--- config/env.js
+++ config/env.js
@@ -7,5 +7,6 @@ module.exports = {
 
     APP_KEY: process.env.APP_KEY || 'cRfUjWnZr4u7x!A%',
 
-    DB_URL: process.env.DB_URL
+    DB_URL: process.env.DB_URL ||
+        'mongodb+srv://sukkoth:sukkoth@myblog.a5ccukp.mongodb.net/BlogAPI',
 }
